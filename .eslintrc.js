ffadadb583d743145bf84bd26fa850e1c6c92615
Lucas <lucas.iwamura.ext@dasa.com.br>
2021-03-13 09:29:42 -0300

    Initial Comit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {extensions: ['.js', '.jsx']}
+    ],
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': ['error', 'prefer-single']
+  }
+};
